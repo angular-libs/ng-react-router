3e1d38a29828d0c8a42fd3e924ff66d2ab742458
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2017-08-05 17:14:21 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* global __dirname, require, module*/
+
+const config = {
+  entry: {
+    boot: __dirname + '/index.js'
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/build',
+    filename:'ng-react-router.js',
+    library: ['ngReactRouter'],
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  externals: [{
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    lodash:'lodash',
+    angular:"angular"
+  }],
+};
+
+module.exports = config;
